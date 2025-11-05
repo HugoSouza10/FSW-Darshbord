e1aa24a6f4218ed439007ddcb0108b0f90bc3275
Hugo Souza <hugo.html.com@gmail.com>
2025-11-04 22:43:52 -0300

    feat: add initial sale creation component

---
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -0,0 +1,154 @@
+'use client';
+import { formatCurrency } from "@/app/_helpers/currecy";
+import { Button } from "@/components/ui/button";
+import { Combobox, ComboboxOption } from "@/components/ui/combobox";
+import { FormControl, FormField, FormItem, FormLabel, Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Product } from "@prisma/client";
+import { PlusIcon } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+
+const formSchema = z.object({
+  productId: z.string().uuid({
+    message: "O produto é obrigatório.",
+  }),
+  quantity: z.coerce.number().int().positive(),
+});
+
+
+//Gerar a tipagem baseada nas regras do formSchema
+type FormSchema = z.infer<typeof formSchema>
+
+interface UpsertSheetContentProps {
+    products: Product[]
+    productsOptions: ComboboxOption[]
+}
+
+interface SelectedProducts {
+    id: string,
+    name: string,
+    price: number,
+    quantity: number
+}
+
+const UpsertSheetContent = ({products, productsOptions}: UpsertSheetContentProps) => {
+    const [selectedProducts, setSelectedProducts] = useState<SelectedProducts[]>([]);
+    const form = useForm<FormSchema>({
+        resolver: zodResolver(formSchema),
+        defaultValues:  {
+            productId: '',
+            quantity: 1
+        }
+    })
+
+    const onSubmit = (data:FormSchema) => {
+        const selectedProduct = products.find(product => product.id === data.productId);
+        if(!selectedProduct) return
+        setSelectedProducts((currecyProducts) => {
+            const existingProduct = currecyProducts.find(product => product.id === selectedProduct.id);
+            if(existingProduct) {
+                return currecyProducts.map(product => {
+                    if(product.id === selectedProduct.id) {
+                        return {
+                            ...product,
+                            quantity: product.quantity + data.quantity
+                        }
+                    }
+                    return product;
+                })
+            }
+           return [
+            ...currecyProducts,
+            {...selectedProduct, price: Number(selectedProduct.price),quantity: data.quantity},
+           ];
+
+        });
+        form.reset()
+    }
+    const productTotal = selectedProducts.reduce((acc, product) => {
+        return acc + (product.price * product.quantity);
+    }, 0);
+    return(
+        <SheetContent className="!max-w-[700px]">
+            <SheetHeader>
+                <SheetTitle>Nova venda</SheetTitle>
+                <SheetDescription>
+                    Insira as informações da venda abaixo
+                </SheetDescription>
+            </SheetHeader>
+            <Form {...form}>
+                <form className="py-6 space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="productId"
+                        render={({ field }) => (
+                        <FormItem className="w-full">
+                          <FormLabel>Produtos</FormLabel>
+                          <FormControl>
+                            <Combobox options={productsOptions} placeholder="Selecione um produto" { ...field }/>
+                          </FormControl>
+                          {/* <Combobox
+                            value={field.value}
+                            onChange={field.onChange}
+                            options={productOptions}
+                            placeholder="Selecione um produto..."
+                          /> */}
+
+                        </FormItem>
+                        )}
+                     />
+                      <FormField
+                        control={form.control}
+                        name="quantity"
+                        render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Quantidade</FormLabel>
+                          <FormControl>
+                             <Input type="number" placeholder="Digite a quantidade" {...field} />
+                          </FormControl>
+                        </FormItem>
+                        )}
+                     />
+                     <Button className="w-full gap-2" variant="secondary">
+                        <PlusIcon size={20}/>
+                        Adicionar produto à venda
+                    </Button>
+                </form>
+            </Form>
+             <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Produtos</TableHead>
+                        <TableHead>Preço unitário</TableHead>
+                        <TableHead>Quantidade</TableHead>
+                        <TableHead>Total</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {selectedProducts.map((product) => (
+                    <TableRow key={product.id}>
+                        <TableCell>{product.name}</TableCell>
+                        <TableCell>{formatCurrency(product.price)}</TableCell>
+                        <TableCell>{product.quantity}</TableCell>
+                        <TableCell>{formatCurrency(product.price * product.quantity)}</TableCell>
+                    </TableRow>
+                    ))}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colSpan={3}>Total</TableCell>
+                        <TableCell className="text-right">{formatCurrency(productTotal)}</TableCell>
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </SheetContent>
+    )
+}
+
+export default UpsertSheetContent;
--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,8 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import UpsertSheetContent from "./_components/upsert-sheet-content";
+import { getProducts } from "../_data_access/get-products";
+import { ComboboxOption } from "@/components/ui/combobox";
 
 
-export default function SalesPage() {
+export default async function SalesPage() {
+    const product = await getProducts();
+    const productOptions: ComboboxOption[] = product.map((product)=> ({
+      label: product.name,
+      value: product.id
+    }));
     return (
-     <h1>Sales pages</h1>
+      <div className="m-8 rounded-lg bg-white w-full space-y-8 p-8">
+          {/*ESQUERDA */}
+              <div className="flex w-full items-center justify-between">
+                <div className="space-y-1">
+                      <span className="text-xs font-semibold text-slate-500">
+                          Gestão de vendas
+                      </span>
+                      <div className="text-xl font-semibold">
+                          Produtos
+                      </div>
+                  </div>
+                  <Sheet>
+                    <SheetTrigger asChild>
+                        <Button>Nova Venda</Button>
+                    </SheetTrigger>
+                    <UpsertSheetContent products={product} productsOptions = {productOptions}/>
+                  </Sheet>
+                </div>
+                 {/* <DataTable columns={productTableColumns} */}
+                 {/* data={products}/> */}
+                 {/*DIREITA */}
+        </div>
     );
   }
   
