87a64a486bce761b1a79d86aa0e2c628f478d55c
Hugo Souza <hugo.html.com@gmail.com>
2026-02-25 15:38:56 -0300

    feat: ensure sale creation is done with a transaction

--- app/_actions/sale/create-sale/index.ts
+++ app/_actions/sale/create-sale/index.ts
@@ -4,51 +4,51 @@ import { createSaleSchema, CreateSaleSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
 export const createSale = async (data: CreateSaleSchema) => {
-  createSaleSchema.parse(data);
-  const sale = await db.sale.create({
-    data: {
-      date: new Date(),
-    },
-  });
+  await db.$transaction(async (trx) => {
+    createSaleSchema.parse(data);
+    const sale = await trx.sale.create({
+      data: {
+        date: new Date(),
+      },
+    });
 
-  // Create SaleProduct e armazenar os produtos vendidos
-  for (const product of data.products) {
-    const productFromDb = (
-      await db.product.findUnique({
+    // Create SaleProduct e armazenar os produtos vendidos
+    for (const product of data.products) {
+      const productFromDb = await db.product.findUnique({
         where: {
           id: product.id,
         },
-      })
-    );
+      });
 
-    if (!productFromDb) {
-      throw new Error("Produto não encontrado");
-    }
-    const productIsOutOfStock = product.quantity > productFromDb.stock;
-    if (productIsOutOfStock) {
-      throw new Error(
-        `Quantidade indisponível em estoque para o produto ${productFromDb.name}.`
-      );
-    }
-    await db.saleProduct.create({
-      data: {
-        saleId: sale.id,
-        productId: product.id,
-        quantity: product.quantity,
-        unitPrice: productFromDb.price,
-      },
-    });
-    //Depois de criar uma venda, atualizar o estoque do produto
-    await db.product.update({
-      where: {
-        id: product.id,
-      },
-      data: {
-        stock: {
-          decrement: product.quantity,
-        }
+      if (!productFromDb) {
+        throw new Error("Produto não encontrado");
       }
-    })
-    revalidatePath("/products");
-  }
+      const productIsOutOfStock = product.quantity > productFromDb.stock;
+      if (productIsOutOfStock) {
+        throw new Error(
+          `Quantidade indisponível em estoque para o produto ${productFromDb.name}.`,
+        );
+      }
+      await trx.saleProduct.create({
+        data: {
+          saleId: sale.id,
+          productId: product.id,
+          quantity: product.quantity,
+          unitPrice: productFromDb.price,
+        },
+      });
+      //Depois de criar uma venda, atualizar o estoque do produto
+      await trx.product.update({
+        where: {
+          id: product.id,
+        },
+        data: {
+          stock: {
+            decrement: product.quantity,
+          },
+        },
+      });
+      revalidatePath("/products");
+    }
+  });
 };
